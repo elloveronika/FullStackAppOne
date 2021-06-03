46317c67f137cad3bdab94b25ee53ffb550764b1
elloveronika <ello.veronika92@gmail.com>
2021-06-03 00:33:05 -0700

    mongoose added

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').parse()
+}
+
 const express = require('express') //import express from the librabry
 const app = express() //calls on the express variable as a method, calls on the function
 const expressLayouts = require('express-ejs-layouts') //requires the layouts packaging as well
@@ -7,9 +11,16 @@ const indexRouter = require('./routes/index') // the slash shows that its relati
 
 app.set('view engine', 'ejs') //
 app.set('views',__dirname + '/views')
-app.set('layout','layouts/layput') // this will access all the layouts at ince instead of it being one by one
+app.set('layout','layouts/layout') // this will access all the layouts at ince instead of it being one by one
 app.use(expressLayouts) //this uses the Layouts variable declared above 
 app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {
+useNewUrlParser : true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 
